a880b9e2da652e63b85d4e9de768bda5ab4d00de
MrKravz <parhomnik443@gmail.com>
2022-09-06 10:15:06 +0300

    Fixed bug commit

--- IdleActionFarm/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ IdleActionFarm/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ namespace Assets.Scripts.PlayerScripts
     [RequireComponent(typeof(Rigidbody), typeof(Animator))]
     public class PlayerMovement : MonoBehaviour
     {
-        [field: SerializeField] public float PlayerSpeed { get; private set; } = default;
+        [field: SerializeField] public float MoveSpeed { get; private set; } = default;
+        [field: SerializeField] public float RotationSpeed { get; private set; } = default;
         [SerializeField] private FloatingJoystick _joystick;
         [SerializeField] private Transform _playerTransform;
         private Rigidbody _playerRb;
@@ -19,15 +20,16 @@ namespace Assets.Scripts.PlayerScripts
             _playerAnim = GetComponent<Animator>();
         }
 
-        private void FixedUpdate()
+        private void Update()
         {
             _playerInput = new Vector3(_joystick.Horizontal, 0f, _joystick.Vertical);
-            _playerMoveVelocity = _playerInput * PlayerSpeed;
-            _playerRb.MovePosition(_playerRb.position + _playerMoveVelocity * Time.deltaTime);
+            _playerMoveVelocity = _playerInput * MoveSpeed;
+            _playerRb.MovePosition(transform.position + transform.forward * _playerMoveVelocity.magnitude * Time.deltaTime);
             if (_playerInput != Vector3.zero)
             {
+                float angle = Vector3.SignedAngle(Vector3.forward, _playerMoveVelocity, Vector3.up) * Time.deltaTime * RotationSpeed;
                 _playerAnim.SetBool("IsRunning", true);
-                _playerRb.MoveRotation(Quaternion.LookRotation(_playerInput, Vector3.up));
+                transform.forward = Quaternion.Euler(0, angle, 0) * transform.forward;
                 return;
             }
             _playerAnim.SetBool("IsRunning", false);
